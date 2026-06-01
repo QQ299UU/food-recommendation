ecc3ef7d0dc759a86aee672e2865c23f7597cbc9
QQ299UU <2109275195@qq.com>
2026-06-01 22:34:28 +0800

    remove vercel.json

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,13 @@
-/**
- * Vercel deploy entry handler, for serverless deployment, please don't modify this file
- */
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import app from './app.js';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req, res);
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import app from '../app.js'
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  // 将 Vercel 的 request/response 适配给 Express
+  return new Promise((resolve) => {
+    app(req as any, res as any, () => {
+      resolve(void 0)
+    })
+  })
 }
+
